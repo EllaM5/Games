7c80b1beed7abef277347ccd373a552abd7ec044
EllaM5 <ellalandis385@gmail.com>
2025-05-12 13:39:24 -0500

    made new instance varibles and set methods

--- Game.java
+++ Game.java
@@ -1,1 +1,46 @@
+public class Game
+{
+    private int score;
+    private int level;
+    private int lives;
+    private String name;
+    public Game(int score, int level, int lives,String name)
+    {
+        this.score = score;
+        this.level = level;
+        this.lives = lives;
+        this.name = name;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+    public int getLevel()
+    {
+        return level;
+    }
+    public int getLives()
+    {
+        return lives;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
 
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
